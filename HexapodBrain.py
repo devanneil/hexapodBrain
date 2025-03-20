450fe6df5e775b48e132c0a2841d877132c2dfc4
Devan Neil <devanneil@mines.edu>
2025-03-20 13:43:10 -0600

    .

--- HexapodBrain.py
+++ HexapodBrain.py
@@ -9,7 +9,7 @@ import board
 from adafruit_pca9685 import PCA9685
 
 # Create the I2C bus interface
-i2c = busio.I2C(board.GP8, board.GP9)
+i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create a PCA9685 object
 pca = PCA9685(i2c)
